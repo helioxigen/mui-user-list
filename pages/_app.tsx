03e446bb435b6344a7863bac83e29bee4cdd7746
Alexander <al.zatulyviter@gmail.com>
2022-06-15 17:21:27 +0300

    move api key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
   return (
-    <Wrapper apiKey="AIzaSyDh7_-Sv-nkSHHjxyol2-thkx5YoBObyhU">
+    <Wrapper apiKey={process.env.GMAPS_API_KEY}>
       <CacheProvider value={emotionCache}>
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
